d5778fe59e6e5622afab6ad3e4ab739271517833
jackieonway <1254825871@qq.com>
2019-10-16 11:23:50 +0800

    delete unused code

--- src/main/java/com/github/jackieonway/validate/annotation/CheckList.java
+++ src/main/java/com/github/jackieonway/validate/annotation/CheckList.java
@@ -8,8 +8,6 @@ package com.github.jackieonway.validate.annotation;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
